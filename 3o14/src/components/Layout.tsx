d6ad1befd108f7386790d3fbed09cbbb99b5f627
faultypointer <faultypointer@proton.me>
2025-01-23 20:45:42 +0545

    layout component

---
+++ 3o14/src/components/Layout.tsx
@@ -0,0 +1,20 @@
+import type { FC } from "hono/jsx";
+
+export const Layout: FC = (props) => (
+  <html lang="en" >
+    <head>
+      <meta charset="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="color-scheme" content="light dark" />
+      <title>3o14 </title>
+      < link
+        rel="stylesheet"
+        href="/static/out.css"
+      />
+      <script src="https://unpkg.com/htmx.org@2.0.4"></script>
+    </head>
+    < body >
+      <main class="container" > {props.children} </main>
+    </body>
+  </html>
+);
