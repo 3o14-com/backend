a3c2c428b6122ebf599ad1aa582ebe03f9c94bea
faultypointer <faultypointer@proton.me>
2025-01-24 16:42:54 +0545

    users schema

--- 3o14/src/db/schema.ts
+++ 3o14/src/db/schema.ts
@@ -1,12 +1,20 @@
-import { check, pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { check, pgTable, text, timestamp, uuid, varchar } from "drizzle-orm/pg-core";
 import type { Uuid } from "../utils/uuid";
 import { sql } from "drizzle-orm";
 
-export const usersTable = pgTable(
+const currentTimestamp = sql`CURRENT_TIMESTAMP`;
+
+export const users = pgTable(
   "users",
   {
     id: uuid("id").$type<Uuid>().primaryKey(),
-    username: text("username").notNull().unique(),
+    email: varchar("email", { length: 254 }).notNull().unique(),
+    username: varchar("username", { length: 254 }).notNull().unique(),
+    preferred_name: varchar("preferred_name", { length: 254 }).unique(),
+    password_hash: text("password_hash").notNull(),
+    bio: text("bio"),
+    created_at: timestamp("created_at").notNull().default(currentTimestamp),
+    updated_at: timestamp("updated_at").notNull().default(currentTimestamp),
   },
   (table) => [
     check(
@@ -15,3 +23,6 @@ export const usersTable = pgTable(
     ),
   ],
 );
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
