54cc0b93d0f4eceeb020fb61b726f8c6c4f6f39e
faulty <faultypointer@proton.me>
2025-02-16 22:30:13 +0545

    bun to pnpm and node

--- flake.nix
+++ flake.nix
@@ -1,37 +1,24 @@
 {
-  description = "A Nix-flake-based Bun development environment";
+  description = "A Nix-flake-based Node.js development environment";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
 
   outputs = { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems (system:
-          f {
-            pkgs = import nixpkgs {
-              inherit system;
-              config.allowUnfree = true;
-            };
-          });
-    in {
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
+        pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.default ]; };
+      });
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+        yarn = (prev.yarn.override { inherit nodejs; });
+      };
+
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {
-          packages = with pkgs; [
-            bun
-            postgresql
-            watchman
-            tailwindcss-language-server
-            hoppscotch
-            ngrok
-            minio
-            pgweb
-          ];
-
-          shellHook = ''
-            export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib";
-          '';
+          packages = with pkgs; [ node2nix nodejs nodePackages.pnpm yarn postgresql ];
         };
       });
     };
