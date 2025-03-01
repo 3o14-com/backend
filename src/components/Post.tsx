705ed7832e43a0dec9672bf8b1604798e8c0dd91
faulty <faultypointer@proton.me>
2025-03-01 12:24:07 +0545

    post using raw content

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -142,7 +142,7 @@ interface PostContentProps {
 }
 
 function PostContent({ post }: PostContentProps) {
-  const contentHtml = post.contentHtml;
+  const contentHtml = post.content;
   return (
     <>
       {post.contentHtml && (
